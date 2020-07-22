764baa588213497213c08ee5d90a8638e04f2ec3
Janak <ruiajanak@gmail.com>
2020-07-22 22:17:24 +0530

    updated assignment_4.py

--- Python/assignment_4.py
+++ Python/assignment_4.py
@@ -4,7 +4,7 @@ def calc():
 	y=np.random.randint(5, 500, size=(20, 1), dtype=np.int32)
 	x=np.random.normal(size=(20,20))
 	xt=x.transpose()
-	xm=np.dot(x,xt)
+	xm=np.dot(xt,x)
 	xm=np.linalg.inv(xm)
 	x_final=np.dot(xm, xt)
 	return np.dot(x_final, y)
